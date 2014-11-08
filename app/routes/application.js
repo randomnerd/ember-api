f67127658380698a8eba3cd88bbfe3a804be651f
random.nerd <someguy4112@gmail.com>
2014-11-08 20:57:35 +0000

    progress

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
  
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  actions: {
+    error: function (error, transition) {
+      console.log(error);
+      return this.transitionTo('index');
+    }
+  }
+});
